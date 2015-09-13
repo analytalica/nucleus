2b83c05852db3fcc922d0b68b4845d3634ece58b
Niko Lazaris <nikolazaris@utexas.edu>
2015-09-13 01:17:58 -0400

    remove clouds from dashboard

--- views/layout.php
+++ views/layout.php
@@ -1,9 +1,7 @@
 <?hh // strict 
 
 final class :nucleus:layout extends :x:element {
-  attribute
-    User user,
-    string controller;
+  attribute User user, string controller;
 
   children (:xhp);
 
@@ -11,17 +9,17 @@ final class :nucleus:layout extends :x:element {
     return
       <html>
         <nucleus:head />
-        <body>
+        <body class={strtolower($this->getAttribute('controller'))}>
           <nucleus:analytics tracking-id={Config::get('GoogleAnalytics')['tracking_id']} />
           <nucleus:nav-bar user={$this->getAttribute('user')} controller={$this->getAttribute('controller')} />
           <nucleus:flash />
-          <nucleus:clouds />
+          <nucleus:clouds controller={$this->getAttribute('controller')} />
           <div class="container">
             <div class="row col-xs-6 col-xs-offset-3">
               <img class="title-img" src="/img/hacktx.svg" />
             </div>
           </div>
-          <div class={'container ' . strtolower($this->getAttribute('controller'))}>
+          <div class="container">
             {$this->getChildren()}
           </div>
         </body>
